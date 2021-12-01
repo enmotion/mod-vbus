1ceb6f8a2b3e205c125f4c17736e2cb7b500486d
enmotion <40427163@qq.com>
2021-12-02 02:31:11 +0800

    finish try

--- test/example.spec.js
+++ test/example.spec.js
@@ -2,11 +2,18 @@ import ModVbus from "../src"
 // console.log(/(^\#|^\+|^(\/|[A-Za-z]){1}(\w+\/|\+{1}\/|\w)+(\w*|\+*|\#*))$/g.test('/root/test'))
 // console.log(/(^\#|^\+|^(\/|[A-Za-z]){1}(\w+\/|\+{1}\/|\w)+(\w*|\+*|\#*))$/g.test('#'))
 // console.log(/(^\#|^\+|^(\/|[A-Za-z]){1}(\w+\/|\+{1}\/|\w)+(\w*|\+*|\#*))$/g.test('##'))
-var v001 = ModVbus.create('v001',['P001'])
-var v002 = ModVbus.create('v002',['P002'])
-var v003 = ModVbus.create('v003',['P003'])
-v001.sub("test");
-v002.pub("P003",{test:"haha"});
-// console.log(v001.getAllSubedTopics())
-v001.unsub("test");
-// console.log(v001.getAllSubedTopics())
+var v001 = new ModVbus('v001',['P001'])
+var v002 = new ModVbus('v002',['P002'])
+var v003 = new ModVbus('v003',['P003'])
+var v003 = new ModVbus('v003',['P003'])
+v001.sub("chartG01/#");
+v003.sub("chartG01/")
+v001.on(function(msg){
+    if(msg.payload.func=="hide"){
+        setTimeout(function(){
+            alert("v001"+'|'+msg.sender+":"+msg.payload.params.name)
+        },1000)
+    }
+})
+v002.pub("chartG01/V004",{func:"hide",params:{name:"v002 call 001"}});
+v003.pub("chartG01/V004",{func:"hide",params:{name:"v003 call 001"}});
